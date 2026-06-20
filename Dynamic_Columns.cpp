7de173a21143c1dad57974a6d2bde5d2ea38fcb1
asimayazmrd0987 <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>
2026-06-20 06:34:49 +0500

    32_mem

--- Dynamic_Columns.cpp
+++ Dynamic_Columns.cpp
@@ -9,7 +9,7 @@ int main()
     brr[0] = vector<int>(4);
     brr[1] = vector<int>(3);
     brr[2] = vector<int>(5);
-    brr[3] = vector<int>(2);
+    brr[3]  = vector<int>(2);
 
     int totalRows = brr.size();
 
